53242986a75d6c1a1a98315778ae8be725162ccb
Pedro <pedrohenry.viana@gmail.com>
2021-07-23 16:17:58 -0300

    feat(tablecomponent): add table component to list information
    Add a table component to show all logged data and also allow you to delete values.
    To simplify
    viewing information and manipulating it.
    It was developed using the chakra ui library.
    
    fixes #3

---
+++ src/components/TableComponents/index.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import { Accordion, Table, Th, Thead, Tr } from '@chakra-ui/react';
+
+import { AccordionComponent } from '../AccordionComponent';
+
+export const TableComponents = () => {
+  const data = [
+    {
+      name: 'Pedro Henrique Viana dos Santos',
+      email: 'pedrohenry.viana@gmail.com',
+      document: '065.601.33-81',
+      fone: '(64) 9 9663-4177',
+      cep: '38010-000',
+      publicPlace: 'Leopoldina de Oliveira',
+      number: '4050',
+      district: 'Centro',
+      city: 'Uberaba',
+      state: 'MG'
+    },
+    {
+      name: 'Arthur Lopes de Morais',
+      email: 'arthurdoflamento@gmail.com',
+      document: '123.456.789-89',
+      fone: '(64) 9 9145-5245',
+      cep: '75890-000',
+      publicPlace: 'Alameda Silva',
+      number: '45',
+      district: 'Santa Lucia',
+      city: 'Itumbiara',
+      state: 'GO'
+    },
+    {
+      name: 'Arthur Lopes de Morais',
+      email: 'arthurdoflamento@gmail.com',
+      document: '123.456.789-89',
+      fone: '(64) 9 9145-5245',
+      cep: '75890-000',
+      publicPlace: 'Alameda Silva',
+      number: '45',
+      district: 'Santa Lucia',
+      city: 'Itumbiara',
+      state: 'GO'
+    },
+    {
+      name: 'Arthur Lopes de Morais',
+      email: 'arthurdoflamento@gmail.com',
+      document: '123.456.789-89',
+      fone: '(64) 9 9145-5245',
+      cep: '75890-000',
+      publicPlace: 'Alameda Silva',
+      number: '45',
+      district: 'Santa Lucia',
+      city: 'Itumbiara',
+      state: 'GO'
+    }
+  ];
+
+  return (
+    <>
+      <Table variant="unstyled">
+        <Thead>
+          <Tr bg="green.700" color="white">
+            <Th w="10%" />
+            <Th fontSize="md" w="45%" fontWeight="bold">
+              Nome
+            </Th>
+            <Th fontSize="md" w="45%" fontWeight="bold">
+              CPF/CNPJ
+            </Th>
+          </Tr>
+        </Thead>
+      </Table>
+
+      <Accordion allowToggle w="full">
+        {data.map(client => (
+          <AccordionComponent key={client.name} data={client} />
+        ))}
+      </Accordion>
+    </>
+  );
+};
